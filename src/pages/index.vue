a6e95627852ffbe8a39b315c4a750b6a7072015f
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-08 03:47:29 +0800

    home

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,9 +6,11 @@ export { stringify } from '../utils/stringify'
 <template lang='pug'>
 .canvas
   div
-    h1.font-mono.mb-2.text-lg 100d.
+    h1.font-mono.mb-2.text-lg 100 days
     pre
       span.text-gray-400 [
       router-link.ml-4.link.block(v-for='work of works' :key='work.no' :to='`/${work.no}`') {{ stringify({ name: work.name, no: work.no }) }},
       span.text-gray-400 ]
+    h1.font-mono.mb-2.text-lg.text-gray-300.mt-8 by
+      a.ml-2.link(href='https://antfu.me' target='_blank') Anthony Fu
 </template>
