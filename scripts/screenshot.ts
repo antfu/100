bdd4506e0ae30c3722a3e0e4f8ed7e7bf053dbaf
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-08 04:19:45 +0800

    add shots

--- scripts/screenshot.ts
+++ scripts/screenshot.ts
@@ -1,21 +1,31 @@
 import { chromium } from 'playwright'
 
-async function run(no: string, take = 3) {
+async function run() {
   const browser = await chromium.launch({ headless: false })
   const context = await browser.newContext({
     viewport: {
-      width: 1200,
-      height: 675,
+      width: 960,
+      height: 540,
     },
     deviceScaleFactor: 2,
   })
-  for (let i = 0; i < take; i++) {
-    const page = await context.newPage()
-    await page.goto(`http://100.antfu.me/${no}?shot=true`)
-    await page.waitForTimeout(1000)
-    await page.screenshot({ path: `scripts/screenshots/${no}-${i}.png` })
+
+  async function take(no: string, retry = 3, delay = 1000, take = 1) {
+    for (let i = 0; i < retry; i++) {
+      const page = await context.newPage()
+      await page.goto(`http://100.antfu.me/${no}?shot=true`)
+      await page.waitForTimeout(delay)
+      for (let j = 0; j < take; j++)
+        await page.screenshot({ path: `scripts/screenshots/${no}-${i}-${j}.png` })
+
+      page.close()
+    }
   }
+
+  // await take('001', 3, 800, 4)
+  await take('002', 8, 500)
+
   await browser.close()
 }
 
-run('001')
+run()
