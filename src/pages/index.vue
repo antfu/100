33c064a22868c9beb7c702737ca25ae428a8cb60
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-09 03:41:09 +0800

    fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,7 +12,7 @@ useTitle('100 days')
     h1.font-mono.mb-2.text-lg 100 days
     pre
       span.text-gray-400 [
-      router-link.ml-4.link.block(v-for='work of works' :key='work.no' :to='`/${work.no}`' v-if='work.draft') {{ stringify({ name: work.name, no: work.no }) }},
+      router-link.ml-4.link.block(v-for='work of works' :key='work.no' :to='`/${work.no}`' v-if='!work.draft') {{ stringify({ name: work.name, no: work.no }) }},
       span.text-gray-400 ]
     h1.font-mono.mb-2.text-lg.text-gray-300.mt-8 by
       a.ml-2.link(href='https://antfu.me' target='_blank') Anthony Fu
