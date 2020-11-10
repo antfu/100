9a8ad93afb898fd3b91bff819354f4660960309b
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-11 02:42:28 +0800

    update

--- src/pages/007.vue
+++ src/pages/007.vue
@@ -1,17 +1,27 @@
 <template lang='pug'>
 paper
-  .box(ref='box')
-    .anchor.overflow-visable.w-0.h-0.fixed
-      .text(:style='transformStyle') A ship in harbor is safe,<br>but that is not what ships are built for
+  div
+    .box(ref='box')
+      .anchor.overflow-visable.w-0.h-0.fixed
+        .text(:style='transformStyle') A ship in harbor is safe,<br>but that is not what ships are built for
 
-    p {{theta}} {{transformStyle}} {{ gamma }} {{ beta }}
+      .div.p-4(v-if='debug')
+        p Gamma {{ gamma }}
+        p Beta {{ beta }}
+        p Theta {{theta}}
+    .text-gray-400.text-center(v-if='!mobile') use phone to visit
 </template>
 
 <script setup lang='ts'>
 import { clamp, useDeviceOrientation } from '@vueuse/core'
 import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+
+export const debug = !!route.query.debug
 export const { gamma, beta } = useDeviceOrientation()
+export const mobile = window.orientation == null
 
 export const theta = computed(() => {
   if (gamma.value == null || beta.value == null)
@@ -20,6 +30,9 @@ export const theta = computed(() => {
   const orientation = window.orientation
   let x = 0
   let y = 0
+
+  if (orientation == null)
+    return 0
 
   if (orientation === 180) {
     x = clamp(gamma.value, -90, 90) / 90
@@ -38,7 +51,7 @@ export const theta = computed(() => {
     y = clamp(beta.value, -90, 90) / 90
   }
 
-  return Math.atan2(y, x)
+  return Math.atan2(y, x) - Math.PI / 2
 })
 
 export const transformStyle = computed(() => {
