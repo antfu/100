8d2bf2144bef221481118f738f73f86f5f96c5bc
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-11 11:04:35 +0800

    info

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,8 +16,10 @@ useTitle('100 days')
       span.text-gray-400 [
       router-link.ml-4.link.block(v-for='work of works' :key='work.no' :to='`/${work.no}`') {{ stringify({ name: work.name, no: work.no }) }},
       span.text-gray-400 ]
-    h1.font-mono.mb-2.text-lg.text-gray-300.mt-8 // by
+    h1.font-mono.mb-2.text-gray-300.mt-8 // by
       a.ml-2.link(href='https://antfu.me' target='_blank') Anthony Fu
       br
       span // from Nov. 6th, 2020
+      br
+      span // updates on <a href='https://twitter.com/antfu7/status/1325977074813739009' target='_blank' class='link'>twitter</a>
 </template>
