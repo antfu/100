959f2c7d48753666b85cd534fa59601c006e16e5
Anthony Fu <anthonyfu117@hotmail.com>
2020-12-01 12:44:07 +0800

    home

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,16 +11,19 @@ useTitle('100 days')
 <template lang='pug'>
 .paper
   .centered
-    h1.font-mono.mb-2.text-lg 100 days
-    pre
-      span.text-gray-400 [
-      router-link.ml-4.link.block(v-for='work of avaliable_works' :key='work.no' :to='`/${work.no}`') {{ stringify({ name: work.name, no: work.no }) }},
-      span.text-gray-400 ]
-    h1.font-mono.mb-2.text-gray-300.mt-8 // by
-      a.ml-2.link(href='https://antfu.me' target='_blank') Anthony Fu
+    h1.font-mono.mb-2.text-lg.font-bold.mb-4 100 days
+    pre.grid.grid-cols-2.gap-x-4.gap-y-1
+      router-link.link.block.mr-4(v-for='work of avaliable_works' :key='work.no' :to='`/${work.no}`')
+        span.mr-2.opacity-50 {{ work.no }}
+        b {{ work.name }}
+    h1.font-mono.mb-2.mt-8
+      a.link(href='https://antfu.me' target='_blank') @antfu
+      span.mx-1 .
+      a.link(href='https://twitter.com/antfu7/status/1325977074813739009' target='_blank') twitter
+      span.mx-1 .
+      a.link(href='https://github.com/antfu/100' target='_blank') github
+      span.mx-1 .
+      a.link(href='https://codecember.ink' target='_blank') codecember
       br
-      span // from Nov. 6th, 2020
-      br
-      span // updates on <a href='https://twitter.com/antfu7/status/1325977074813739009' target='_blank' class='link'>twitter</a>
-      span // source on <a href='https://github.com/antfu/100' target='_blank' class='link'>github</a>
+      span.text-gray-300 from 2020/11/06
 </template>
