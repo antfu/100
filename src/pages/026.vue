23996f5b5a0dd116abf48053e6fe78fac39de7b8
Anthony Fu <anthonyfu117@hotmail.com>
2020-12-02 15:36:07 +0800

    p5i

--- src/pages/026.vue
+++ src/pages/026.vue
@@ -7,8 +7,9 @@ note
 </template>
 
 <script setup lang="ts">
-import P5 from 'p5'
-import { onMounted, ref } from 'vue'
+import { createP5 } from 'p5i'
+import type { P5i } from 'p5i'
+import { onMounted, onUnmounted, ref } from 'vue'
 import { distance, r60, SQRT_3 } from '../utils'
 
 const el = ref<HTMLCanvasElement | null>(null)
@@ -29,81 +30,71 @@ const n = 10
 const cx = w / 2
 const cy = h / 2
 
-function fuckP5(p5: P5): P5 {
-  const fucked = {} as P5
+const { createCanvas, background, strokeWeight, map, stroke, push, translate, pop, line, rotate, fill, noStroke, mount, unmount } = createP5()
 
-  // eslint-disable-next-line
-  for (const k in p5) {
-    const v = p5[k]
-    if (!k.startsWith('_') && typeof v === 'function')
-      fucked[k] = v.bind(p5)
-    else
-      fucked[k] = v
+function setup() {
+  createCanvas(w, h)
+}
+
+function draw({ mouseX, mouseY }: P5i) {
+  background('transparent')
+
+  for (let i = 0; i < rows - 1; i++) {
+    for (let j = 0; j < columns - 1; j++) {
+      const q = i - rows / 2
+      const r = j - columns / 2
+      const s = -q - r
+
+      if (abs(q) > n || abs(r) > n || abs(s) > n)
+        continue
+
+      // https://www.redblobgames.com/grids/hexagons/#hex-to-pixel-axial
+      const currentOffset = {
+        x: cx + margin * (q * SQRT_3 + r * SQRT_3 / 2),
+        y: cx + margin * (r * 3 / 2),
+      }
+
+      const d = distance([currentOffset.x, currentOffset.y], [mouseX, mouseY])
+      const gravity = 1 - min(d / 400, 1)
+
+      const length = len * gravity
+      const delta = {
+        x: currentOffset.x + length / 2 - margin / 2 - mouseX,
+        y: currentOffset.y - mouseY,
+      }
+
+      let theta = atan2(delta.y, delta.x)
+      const deltaThreshold = 40
+
+      theta += r60 * gravity
+
+      strokeWeight(strokeWidth)
+      stroke(100)
+
+      if (abs(delta.x) < deltaThreshold && abs(delta.y) < deltaThreshold) {
+        const amt = (abs(delta.x) + abs(delta.y)) / 2
+        const amtMapped = map(amt, 0, deltaThreshold, -50, 255)
+        stroke(100, amtMapped)
+      }
+
+      push()
+      translate(currentOffset.x, currentOffset.y)
+      rotate(theta)
+
+      line(0, 0, length, 0)
+      pop()
+
+      fill(250, 150, 0)
+      noStroke()
+    }
   }
-  return fucked
 }
 
 onMounted(() => {
-  new P5((p5: P5) => {
-    const { createCanvas, background, strokeWeight, stroke, push, translate, pop, line, rotate, fill, noStroke } = fuckP5(p5)
+  mount(el.value!, { setup, draw })
+})
 
-    p5.setup = () => {
-      createCanvas(w, h)
-    }
-
-    p5.draw = () => {
-      background('transparent')
-
-      for (let i = 0; i < rows - 1; i++) {
-        for (let j = 0; j < columns - 1; j++) {
-          const q = i - rows / 2
-          const r = j - columns / 2
-          const s = -q - r
-
-          if (abs(q) > n || abs(r) > n || abs(s) > n)
-            continue
-
-          // https://www.redblobgames.com/grids/hexagons/#hex-to-pixel-axial
-          const currentOffset = {
-            x: cx + margin * (q * SQRT_3 + r * SQRT_3 / 2),
-            y: cx + margin * (r * 3 / 2),
-          }
-
-          const d = distance([currentOffset.x, currentOffset.y], [p5.mouseX, p5.mouseY])
-          const gravity = 1 - min(d / 400, 1)
-
-          const length = len * gravity
-          const delta = {
-            x: currentOffset.x + length / 2 - margin / 2 - p5.mouseX,
-            y: currentOffset.y - p5.mouseY,
-          }
-
-          let theta = atan2(delta.y, delta.x)
-          const deltaThreshold = 40
-
-          theta += r60 * gravity
-
-          strokeWeight(strokeWidth)
-          stroke(100)
-
-          if (abs(delta.x) < deltaThreshold && abs(delta.y) < deltaThreshold) {
-            const amt = (abs(delta.x) + abs(delta.y)) / 2
-            const amtMapped = p5.map(amt, 0, deltaThreshold, -50, 255)
-            stroke(100, amtMapped)
-          }
-
-          push()
-          translate(currentOffset.x, currentOffset.y)
-          rotate(theta)
-
-          line(0, 0, length, 0)
-          pop()
-
-          fill(250, 150, 0)
-          noStroke()
-        }
-      }
-    }
-  }, el.value!)
+onUnmounted(() => {
+  unmount()
 })
 </script>
