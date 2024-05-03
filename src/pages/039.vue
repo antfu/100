7553bdafeaf9e4d611c128621256da36c62eaec3
Anthony Fu <anthonyfu117@hotmail.com>
2024-05-03 23:27:24 +0200

    feat: improve canvas

--- src/pages/039.vue
+++ src/pages/039.vue
@@ -1,40 +1,52 @@
-<template lang='pug'>
-paper
-  .fixed.left-0.right-0.bottom-0.top-0(ref='el' style='z-index:-1')
-</template>
-
 <script setup lang="ts">
 import { p5i } from 'p5i'
 import type { P5I } from 'p5i'
-import { onMounted, onUnmounted, ref, watch } from 'vue'
-import { transform } from 'lodash-es'
+import { onMounted, onUnmounted, ref } from 'vue'
 
 const el = ref<HTMLCanvasElement | null>(null)
 
-const { random, trunc, min } = Math
 const {
-  mount, unmount,
-  createCanvas, resizeCanvas, background, noFill,
-  stroke, noise, noiseSeed, noLoop, cos, sin, line,
-  beginShape, endShape, vertex,
-  push, pop, translate,
+  mount,
+  unmount,
+  createCanvas,
+  background,
+  noFill,
+  stroke,
+  noise,
+  noiseSeed,
+  resizeCanvas,
+  cos,
+  sin,
   TWO_PI,
 } = p5i()
 
-const w = window.innerWidth
-const h = window.innerHeight
+let w = window.innerWidth
+let h = window.innerHeight
+let offsetY = window.scrollY
 
 const SCALE = 200
 const LENGTH = 10
 const SPACING = 15
-const AMOUNT = 2000
 
 function getForceOnPoint(x: number, y: number, z: number) {
   // https://p5js.org/reference/#/p5/noise
   return (noise(x / SCALE, y / SCALE, z) - 0.5) * 2 * TWO_PI
 }
 
-let points: {x: number; y: number }[] = []
+const existingPoints = new Set<string>()
+const points: { id: string, x: number, y: number }[] = []
+
+function addPoints() {
+  for (let x = -SPACING / 2; x < w + SPACING; x += SPACING) {
+    for (let y = -SPACING / 2; y < h + offsetY + SPACING; y += SPACING) {
+      const id = `${x}-${y}`
+      if (existingPoints.has(id))
+        continue
+      existingPoints.add(id)
+      points.push({ id, x, y })
+    }
+  }
+}
 
 function setup() {
   createCanvas(w, h)
@@ -44,15 +56,10 @@ function setup() {
 
   noiseSeed(+new Date())
 
-  points = []
-
-  for (let x = SPACING / 2; x < w; x += SPACING) {
-    for (let y = SPACING / 2; y < h; y += SPACING)
-      points.push({ x, y })
-  }
+  addPoints()
 }
 
-function draw({ frameCount, mouseX, mouseY, millis, circle }: P5I) {
+function draw({ circle }: P5I) {
   background('white')
   const t = +new Date() / 10000
 
@@ -62,7 +69,7 @@ function draw({ frameCount, mouseX, mouseY, millis, circle }: P5I) {
     const length = (noise(x / SCALE, y / SCALE, t * 2) + 0.5) * LENGTH
     const nx = x + cos(rad) * length
     const ny = y + sin(rad) * length
-    circle(nx, ny, 1)
+    circle(nx, ny - offsetY, 1)
   }
 }
 
@@ -71,6 +78,25 @@ function restart() {
     mount(el.value, { setup, draw })
 }
 
-onMounted(restart)
+onMounted(() => {
+  restart()
+
+  useEventListener('resize', () => {
+    w = window.innerWidth
+    h = window.innerHeight
+    resizeCanvas(w, h)
+    addPoints()
+  })
+
+  useEventListener('scroll', () => {
+    offsetY = window.scrollY
+    addPoints()
+  }, { passive: true })
+})
 onUnmounted(() => unmount())
 </script>
+
+<template lang='pug'>
+paper
+  .fixed.left-0.right-0.bottom-0.top-0(ref='el' style='z-index:-1')
+</template>
